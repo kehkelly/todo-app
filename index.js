66111025be9d978394372f318f10c0a9721bab44
Kelly Dantas <kdsl.dantas2017@gmail.com>
2023-11-27 18:36:34 -0300

    criação - primeira view do projeto

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express")
-const exphbs = require("express-hanlebars")
+const exphbs = require("express-handlebars")
 const app = express()
 
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send("Olá mundo!")
+    resposta.render('home')
 })
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000!")
