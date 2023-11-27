8f6ccec701603e5b446bd494c9bde928dc994739
Kelly Dantas <kdsl.dantas2017@gmail.com>
2023-11-27 18:14:17 -0300

    Instalação do handlebars e do nodemon

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express")
+const exphbs = require("express-hanlebars")
 const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
 app.get('/', (requisicao, resposta) => {
     resposta.send("Olá mundo!")
 })
