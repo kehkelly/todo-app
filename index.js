17dcb09d588f8aacaa8b8b1dac1a31c7f02d2bb0
Kelly Dantas <kdsl.dantas2017@gmail.com>
2023-11-29 16:31:55 -0300

    Iniciando a conexão com o mysql

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
+const mysql = require("mysql2")
+
 const app = express()
 
 app.engine('handlebars', exphbs.engine())
@@ -10,6 +12,15 @@ app.use(express.static('public'))
 app.get('/', (requisicao, resposta) => {
     resposta.render('home')
 })
+
+const conexao = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "ke013154819761",
+    database: "todoapp",
+    port: 3306
+})
+
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000!")
 })
